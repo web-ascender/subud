a873961fdbaa231ea08d71a8b72c7bf315e3a2c5
Ryan Doom <ryan.doom@webascender.com>
2013-01-16 15:18:06 -0500

    region create

--- db/migrate/20130116195600_add_region_assoc.rb
+++ db/migrate/20130116195600_add_region_assoc.rb
@@ -1,5 +1,9 @@
 class AddRegionAssoc < ActiveRecord::Migration
   def up
+    RegionAssociation.create(name: "I am the contact person for information in this region")
+    RegionAssociation.create(name: "I am the contact helper for information in this region")
+    RegionAssociation.create(name: "I am the contact person for information in this center")
+    RegionAssociation.create(name: "I am the contact helper for information in this center")
   end
 
   def down
